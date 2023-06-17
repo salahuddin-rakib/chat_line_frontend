f7510ae794c2c8d7e478232e913557054e9839d3
gazi_salahuddin <gsalahuddin@relisource.com>
2023-06-18 02:30:46 +0600

    Email and password validation.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {POST} from "../api/axios.jsx";
 import {useNavigate} from 'react-router-dom';
 
@@ -7,6 +7,8 @@ const Login = () => {
   const initialLoginInfo = {email: '', password: ''};
   const [loginInfo, setLoginInfo] = useState(initialLoginInfo);
   const [response, setResponse] = useState({status: '', message: ''});
+  const [emptyEmail, setEmptyEmail] = useState(false);
+  const [emptyPassword, setEmptyPassword] = useState(false);
 
   useEffect(() => {
     setResponse({status: '', message: ''});
@@ -14,22 +16,31 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const response = await POST(`/users/login`, {...loginInfo});
-      console.log("Printing response: ", response);
-      window.localStorage.setItem('auth_token', response?.data?.token);
-      window.localStorage.setItem('username', response?.data?.username);
-      window.location.href = `/home`
-      setLoginInfo(initialLoginInfo);
-    } catch (error) {
-      if (!error) {
-        setResponse({status: 'error', message: 'No server response'})
-      } else if (error.response?.status === 400) {
-        setResponse({status: 'error', message: 'Missing username and/or password.'})
-      } else if (error.response?.status === 401) {
-        setResponse({status: 'error', message: 'Unauthorized.'})
-      } else {
-        setResponse({status: 'error', message: 'Login Failed.'})
+    if (loginInfo.email === '') {
+      setEmptyEmail(true);
+    } else if (loginInfo.password === '') {
+      setEmptyEmail(false);
+      setEmptyPassword(true);
+    } else {
+      setEmptyEmail(false);
+      setEmptyPassword(false);
+      try {
+        const response = await POST(`/users/login`, {...loginInfo});
+        console.log("Printing response: ", response);
+        window.localStorage.setItem('auth_token', response?.data?.token);
+        window.localStorage.setItem('username', response?.data?.username);
+        window.location.href = `/home`
+        setLoginInfo(initialLoginInfo);
+      } catch (error) {
+        if (!error) {
+          setResponse({status: 'error', message: 'No server response'})
+        } else if (error.response?.status === 400) {
+          setResponse({status: 'error', message: 'Missing username and/or password.'})
+        } else if (error.response?.status === 401) {
+          setResponse({status: 'error', message: 'Unauthorized.'})
+        } else {
+          setResponse({status: 'error', message: 'Login Failed.'})
+        }
       }
     }
   }
@@ -39,6 +50,8 @@ const Login = () => {
       {response.status === 'error' && <p>{response.message}</p>}
       <h2>Login</h2>
       <form className="login-form" onSubmit={handleSubmit}>
+        { emptyEmail && <p style={{color: 'red'}}>Email is needed.</p>}
+        { emptyPassword && <p style={{color: 'red'}}>Password is needed.</p>}
         <label htmlFor="email">Email</label>
         <input
           value={loginInfo.email}
